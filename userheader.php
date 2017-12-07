a3c255df64e12584a4db3c78d2e82d3bcb7a0967
beaegido <32574126+beaegido@users.noreply.github.com>
2017-12-07 21:23:50 +0000

    Create userheader.php

---
+++ userheader.php
@@ -0,0 +1,19 @@
+<?php 
+<header>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a href="dashboard.php"><img src="http://placehold.it/320x80&text=logo" alt="company name"></a>
+    </div>
+    <ul class="nav navbar-nav" style="float:right;">
+      <li><a href="postyourownclass.php">Post your own class</a></li>
+      <li><a href="reserve.php">Reserve a class</a></li>
+      <li><a href="#">Location</a></li>
+      <li><a href="#">Contact Us</a></li>
+	  <li><a href="logout.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a> </li>
+    </ul>
+  </div>
+</nav> 
+ 
+</header>
+?>	
