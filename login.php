c1e21fbc22658579f44edb7c87b672f59c8fb557
waynehsu9598 <32574015+waynehsu9598@users.noreply.github.com>
2017-12-14 08:18:24 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once 'dbconnect.php';
 require_once 'testinput.php';
 session_start();
@@ -12,16 +11,25 @@ $password = test_input($_GET['password']);
 $connection = connect();
 //SQL to find the email
 $sql = "SELECT * FROM user WHERE username='$username' AND password='$password' ";
-
 //Execute query and get the result
 $result = mysqli_query($connection, $sql);
-
 //Get the first row of the result as an array
 $row = mysqli_fetch_array($result);
-
 //If the user doesn't exist there will be no rows in the $result
+
+$sql2 = "SELECT * FROM administrator WHERE username='$username' AND password='$password' ";
+$result2 = mysqli_query($connection, $sql2);
+$row2 = mysqli_fetch_array($result2);
+
+
 if (mysqli_num_rows($result) === 0) {
-    $error = "Username and password combination not valid";
+    if(mysqli_num_rows($result2) === 0) {
+        $error = "Username and password combination not valid";
+    } else {
+                $_SESSION['administrator.username']=$username;
+		header("Location: administrator.php");
+    }
+    
 }  else {	
 		$_SESSION['username']=$username;
 		header("Location: home_page.php");
