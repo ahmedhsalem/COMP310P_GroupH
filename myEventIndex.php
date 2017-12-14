ac3cbbf2d7d3b9d080765a67a5125433bb06b74b
waynehsu9598 <32574015+waynehsu9598@users.noreply.github.com>
2017-12-14 13:09:38 +0800

    Add files via upload

--- myEventIndex.php
+++ myEventIndex.php
@@ -1,5 +1,4 @@
 <?php
-
 $query1=("SELECT DISTINCT *, first_name FROM `requested_event`
       JOIN ticket ON requested_event.event_id = ticket.event_id
       JOIN user ON ticket.user_id=user.user_id
@@ -19,26 +18,22 @@ $query1=("SELECT DISTINCT *, first_name FROM `requested_event`
       //event hosting
       $query3=("SELECT * FROM requested_event WHERE requested_event.user_id = '$userid_session'");
       $result3=mysqli_query($connection, $query3);
-
       
-      if (mysqli_fetch_array($result1) > 0) {
-                        while($row1 = mysqli_fetch_array($result1)) {
+      if ($result1->num_rows > 0) {
+                        while($row1 = $result1->fetch_assoc()) {
                             $attendedList = $row1['event_name']."<br>";
                         }
                     } else {
                         $attendedList = "0 results";
                     }
-       
-       if (mysqli_fetch_array($result2) > 0) {
-                        while($row2 = mysqli_fetch_array($result2)) {
+       if ($result2->num_rows > 0) {
+                        while($row2 = $result2->fetch_assoc()) {
                             $attendingList = $row2['event_name']."<br>";
                         }
                     } else {
                         $attendingList = "0 results";
                     }
-    
-      
-            
+
                 
          
    ?>
