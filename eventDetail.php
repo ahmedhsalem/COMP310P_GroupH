ef0dc026b83eee43f6fabaccf9d6334d6ad7f8fb
waynehsu9598 <32574015+waynehsu9598@users.noreply.github.com>
2017-12-15 23:25:16 +0800

    Add files via upload

--- eventDetail.php
+++ eventDetail.php
@@ -1,15 +1,17 @@
 <?php
       require_once('session.php');
-      ob_start();
-      require_once ('eventList.php');
-      ob_end_clean();
       require_once('eventDetailIndex.php');
       require ('eventListIndex.php');
       require_once ('feedback.php');
+      require_once 'initialise.php';
+      ob_start();
+      require_once ('eventList.php');
+      ob_end_clean();
 ?>
 <!DOCTYPE html>
 <head>
   <link href="layout.css" rel="stylesheet" type="text/css"/>
+  <title>Event Detail</title>
   <script>
   $(function() {
     $('form').submit(function() {
@@ -61,37 +63,7 @@ height: 1400px;
 }
 </style>
 <body>
-	<div id="wrapper">
-	<div id="banner">
-	<a href="home_page.php">
-	<img src="logo.png"
-	width="20%" height="70px"
-	align="left"></a> 
-	</div>
-	<div id="menuTop">
-		<header id="page_header">
-			<nav>
-				<ul>
-					<li><a href="home_page.php" class="links">Home</a>
-					<li><a href="index.html" class="links">Events</a>
-					<li><a href="DVD_Rental_Page.html" class="links">Locations</a>
-					<li><a href="request_event_page.php" class="links">Request an Event</a>
-					<li><a href="DVD_Returns.html" class="links">Contact Us</a>
-				</ul>
-			</nav>
-		</header>
-		<form action="search_results_page.php">
-		<input type="submit" value="Go" id="go"/>
-		<input type="text" id="search" class="search"/>
-		<label for="search" id="label">Search:</label>
-		</form>
-	</div>
-	<div id="columnRight">
-	Welcome, <?php echo " $login_session"; ?> <br/>
-	<a href="myEvent.php">My Events</a><br/>
-	<a href="settings_page.php">Settings</a><br/>
-	<a href="logout.php"><button type="button">Logout</button></a>
-	</div>
+	
 	<div id="content">
 	<div class="innercontent">
             <div id='columnHeading'>
