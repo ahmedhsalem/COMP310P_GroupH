e4d557fd30c1e3a2113a29ee7a77a6b16d58c8ee
beaegido <32574126+beaegido@users.noreply.github.com>
2017-12-15 08:40:26 +0000

    Update search_event_page.php

--- search_event_page.php
+++ search_event_page.php
@@ -53,11 +53,11 @@ include_once 'session.php';
 			}
 		}
 	
-	//
+	//need to filter for only approved
 	$orderby = " ORDER BY id DESC"; 
 	$sql = "SELECT requested_event.event_name, requested_event.description, requested_event.category_id, category.category_name
 FROM requested_event
-LEFT JOIN category ON requested_event.category_id=category.category_id WHERE approved=1" . $queryCondition;
+LEFT JOIN category ON requested_event.category_id=category.category_id" . $queryCondition;
 	$result = mysqli_query($connection,$sql);
 
 ?>
@@ -130,36 +130,29 @@ height: 700px;
 	
 	
 	
-	
+	 
 	 <div>      
 			<form name="frmSearch" method="post" action="search_event_page.php">
 			<input type="hidden" id="advance_search_submit" name="advance_search_submit" value="<?php echo $advance_search_submit; ?>">
 			<div class="search-box">
 				
-				
-				
+
 				<h2>Search for Events:</h2>
 				<div>
 					
 					
-				
-					
-					
 					<label class="search-label">Use Keywords and filters to find the event you're looking for:</label>
-					
-					
-					
 					<br />
 					<br />
 					<div style="float:left">
+						<!--Filter search box -->
 						<select name="search[search_in]" id="search_in" class="demoInputBox">
 							<option value="">Filters</option>
 							<option value="event_name" <?php if($search_in=="event_name") { echo "selected"; } ?>>Name of Event</option>
 							<option value="description" <?php if($search_in=="description") { echo "selected"; } ?>>Description</option>
 							<option value="category_id" <?php if($search_in=="category_id") { echo "selected"; } ?>>Category</option>
 						</select>
-					</div>
-					
+					</div>	
 					<br />
 					<br />
 					<input type="text" name="search[with_any_one_of]" id="with_any_one_of" class="demoInputBox" value="<?php echo $with_any_one_of; ?>"	/>
@@ -174,6 +167,7 @@ height: 700px;
 			</div>
 			</form>	
 			<?php while($row = mysqli_fetch_assoc($result)) { ?>
+		 	<!--Shows results -->
 			<div>
 				<div><strong><?php echo $row["event_name"]; ?></strong></div>
 				<div class="result-description"><?php echo $row["description"]; ?></div>
