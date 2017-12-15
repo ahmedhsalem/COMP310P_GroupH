a61cd2d6c0b980c353c59f7c0d4c17ef0d14e9a2
waynehsu9598 <32574015+waynehsu9598@users.noreply.github.com>
2017-12-15 19:55:04 +0800

    Add files via upload

--- administratorIndex.php
+++ administratorIndex.php
@@ -2,11 +2,11 @@
 
       $query1=("SELECT * FROM requested_event WHERE approved = 0");
       $result1 = mysqli_query($connection, $query1);
-      $row1 = mysqli_fetch_array($result1);
-      $unapprovedEventName = $row1['event_name'];
-      $unapprovedEventDescription = $row1['description'];
-      $unapprovedTicketPrice = $row1['ticket_price'];
-      $unapprovedEventStartDate = $row1['event_start_date_time'];
-      $unapprovedEventEndDate = $row1['event)end_date_time'];
+//      $row1 = mysqli_fetch_array($result1);
+//      $unapprovedEventName = $row1['event_name'];
+//      $unapprovedEventDescription = $row1['description'];
+//      $unapprovedTicketPrice = $row1['ticket_price'];
+//      $unapprovedEventStartDate = $row1['event_start_date_time'];
+//      $unapprovedEventEndDate = $row1['event)end_date_time'];
 
 ?>
