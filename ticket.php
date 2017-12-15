13967b363b95d7965dda8e1bcc9b5e178e947d50
ahmedhsalem <32573965+ahmedhsalem@users.noreply.github.com>
2017-12-15 00:14:34 +0000

    Add files via upload

--- ticket.php
+++ ticket.php
@@ -9,12 +9,24 @@
 <!DOCTYPE html>
 <head>
   <link href="layout.css" rel="stylesheet" type="text/css"/>
+  <title>Buy Tickets</title>
 </head>
 <style>
    
 .title {
 	font-size: 1.2em;
 	font-weight: bold;
+}
+.button {
+  font: bold 11px Arial;
+  text-decoration: none;
+  background-color: #EEEEEE;
+  color: #333333;
+  padding: 5px 10px 5px 10px;
+  border-top: 1px solid #CCCCCC;
+  border-right: 1px solid #333333;
+  border-bottom: 1px solid #333333;
+  border-left: 1px solid #CCCCCC;
 }
 </style>
 <body>
@@ -54,42 +66,50 @@
             <div id='columnHeading'>
                 <h2>Ticket</h2>
             </div>
-            <div id="contentColumn">
+
                 <p1>Event Name: </p1>
                 <?php echo $eventName;?>
                 <br/><p2>Ticket Price: </p2>
                 <?php echo $ticketPrice;?>
                 <br/><p3>Available Tickets: </p3>
                 <?php echo $totalTicket-$soldTicket;?>
+<!-- Number of Tickets is Limited to 10 tickets at a time and limited to the amount of tickets available --!>     
+				<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">           
                 <br/><p4>Number of Tickets you would like to purchase:</p4>
-					<select id="purchase" name="purchase">\
-								  <option selected="selected">Please Select</option>
-								  <option value="1">1</option>
-								  <option value="2">2</option>
-								  <option value="3">3</option>
-								  <option value="4">4</option>
-								  <option value="5">5</option>
+					<select id="purchase" name="purchase" onChange="totalPrice()">
+							<option selected="selected" value="0">Please Select</option>
+							<?php
+							for ($i = 1;$i <=($totalTicket-$soldTicket);$i++)
+							{
+								echo "<option value='$i'>$i</option>";
+								if ($i==10){
+								break;
+								}
+							}
+							?>
+								
 					</select>
+				<br/><p4>Total Price:</p4><p4 id="price"></p4>
                 <br/><h4>Time Left to Buy Tickets: </h4>
-                <?php require_once ('countDownTimer.php'); ?>
-                
+                <h3><?php require_once ('countDownTimer.php'); ?></h3>
                 <?php
-               
                 if(date('Y-m-d h:i:s')>$ticketEndTime || $totalTicket-$soldTicket==0){
-                   echo "<br/><br/><br/>Sorry the ticket is unavailable.";
+                   echo "<br/><br/><br/>Sorry these tickets are no longer available.";
                 } else {
-                    
                     $SUBMIT = "SUBMIT";
-                    echo "<br/><br/><a href='ticketConfirmation.php'>".$SUBMIT."</a></br>";
-                    
-                }
-                
+                    echo "<input type='button' value='$SUBMIT'/></input>";
+                    }
                 ?>
-               
-            </div>
+               </form>
+
 	</div>
 	</div>
-<script type="text/javascript" src="settings_checker.js">
+<script type="text/javascript">
+function totalPrice (){ 
+var selected = document.getElementById("purchase");
+var number = selected.options[selected.selectedIndex].value;
+document.getElementById("price").innerHTML = " ".concat(number*"<?php echo $ticketPrice;?>");
+}
 </script>
 </body>
 </html>
